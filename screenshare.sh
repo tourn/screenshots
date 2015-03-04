8ce1136423165ae31d6c1028640664be12eccb78
tourn <latzer.daniel@gmail.com>
2015-03-04 16:47:23 +0100

    Fix script not working outside the repo

--- screenshare.sh
+++ screenshare.sh
@@ -7,6 +7,7 @@ IMAGEPATH=$DIR/$FILENAME
 #no slashes at the end!
 REPO=https://tourn.github.io/screenshots
 
+cd $DIR
 scrot $IMAGEPATH
 git add $IMAGEPATH && git commit -m "$FILENAME" && git push && echo $REPO/$FILENAME | xsel -b
 
